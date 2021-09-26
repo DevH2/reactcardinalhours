b73ab3e3831e7524d4c5229cf07deddce0983e33
Devin <devhuey9@gmail.com>
2021-09-26 14:22:18 -0700

    Same thing as last but not in the fetch

--- src/Components/DisplayUsers.jsx
+++ src/Components/DisplayUsers.jsx
@@ -19,19 +19,17 @@ export class DisplayUsers extends Component {
         try {
             const usersResponse = await fetch('https://hours.lren.cf/users/getusers')
             usersResponse.json().then(data => {
-            this.setState({
-                users: data.sort((user1, user2) => {return user2.timeIn-user1.timeIn}).sort(user => {
-                    return user.signedIn===1 ? -1:1
-                })
-            })
-            return data;
-        })                
+                this.setState({users: data})
+                return data;
+            })                
         } catch {
             console.log("Couldn't retrieve users") //turn into snackbar later
         }        
     }
     render() {
-        const filteredUsers = this.state.users.filter(user => user.name.toLowerCase().includes(this.props.searchText.toLowerCase()))
+        const filteredUsers = this.state.users.filter(user => user.name.toLowerCase().includes(this.props.searchText.toLowerCase())).sort((user1, user2) => {return user2.timeIn-user1.timeIn}).sort(user => {
+            return user.signedIn===1 ? -1:1
+        })
         return (
             <div className={"display-users"}>
                 <div>
